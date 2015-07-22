8e44bbb7dbb2fa3fbab7c9dee897a7cc684990d6
ofreax <bertrand.gouny@gmail.com>
2015-07-22 23:01:56 +0200

    new baseimage

--- image/service-available/syslog-ng-core/install.sh
+++ image/service-available/syslog-ng-core/install.sh
@@ -4,11 +4,11 @@
 LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends syslog-ng-core
 
 mkdir -p /var/lib/syslog-ng
-cp /osixia/service-available/syslog-ng-core/assets/config/syslog_ng_default /etc/default/syslog-ng
+cp /osixia/service/syslog-ng-core/assets/config/syslog_ng_default /etc/default/syslog-ng
 touch /var/log/syslog
 chmod u=rw,g=r,o= /var/log/syslog
-cp /osixia/service-available/syslog-ng-core/assets/config/syslog-ng.conf /etc/syslog-ng/syslog-ng.conf
+cp /osixia/service/syslog-ng-core/assets/config/syslog-ng.conf /etc/syslog-ng/syslog-ng.conf
 
 ## Install syslog to "docker logs" forwarder.
 mkdir /etc/service/syslog-forwarder
-ln -s /osixia/service-available/syslog-ng-core/daemon-syslog-forwarder.sh /etc/service/syslog-forwarder/run
+ln -s /osixia/service/syslog-ng-core/daemon-syslog-forwarder.sh /etc/service/syslog-forwarder/run
