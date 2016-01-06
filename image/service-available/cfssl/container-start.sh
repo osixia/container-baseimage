1e1ddf28b805f11d1d0347755ddd22a7d357f116
ofreax <bertrand.gouny@gmail.com>
2016-01-06 21:04:20 +0100

    all in tool/run

--- image/service-available/cfssl/container-start.sh
+++ image/service-available/cfssl/container-start.sh
@@ -5,8 +5,8 @@ FIRST_START_SETUP_DONE="/container/run/state/cfssl-first-start-setup-done"
 # container first start setup
 if [ ! -e "$FIRST_START_SETUP_DONE" ]; then
 
-  chmod 700 ${SERVICE_DIR}/cfssl/assets/tool/*
-  ln -s ${SERVICE_DIR}/cfssl/assets/tool/* /usr/sbin
+  chmod 700 ${SERVICE_DIR}/.cfssl/assets/tool/*
+  ln -s ${SERVICE_DIR}/.cfssl/assets/tool/* /usr/sbin
 
   touch $FIRST_START_SETUP_DONE
 fi
--- image/service-available/logrotate/container-start.sh
+++ image/service-available/logrotate/container-start.sh
@@ -5,7 +5,7 @@ FIRST_START_SETUP_DONE="/container/run/state/logrotate-first-start-setup-done"
 # container first start setup
 if [ ! -e "$FIRST_START_SETUP_DONE" ]; then
 
-  ln -s ${SERVICE_DIR}/logrotate/assets/config/logrotate_syslogng /etc/logrotate.d/syslog-ng
+  ln -s ${SERVICE_DIR}/.logrotate/assets/config/logrotate_syslogng /etc/logrotate.d/syslog-ng
 
   touch $FIRST_START_SETUP_DONE
 fi
