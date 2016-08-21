69a2acfa459366f24b5d1dc3622ee0be358a24b3
ofreax <bertrand.gouny@gmail.com>
2016-08-21 18:55:24 +0200

    fix touch all cron tabs to avoid NUMBER OF HARD LINKS > 1 error

--- image/service-available/:cron/startup.sh
+++ image/service-available/:cron/startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 log-helper level eq trace && set -x
 
+# prevent NUMBER OF HARD LINKS > 1 error
+# https://github.com/phusion/baseimage-docker/issues/198
 touch /etc/crontab /etc/cron.d /etc/cron.daily /etc/cron.hourly /etc/cron.monthly /etc/cron.weekly
 
 find /etc/cron.d/ -exec touch {} \;
