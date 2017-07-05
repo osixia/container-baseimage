7ad431f3670115a76d450a7b6a761c99b355f888
Bertrand Gouny <bertrand.gouny@osixia.net>
2017-07-05 10:19:29 +0200

    add some logs and fix shutdown timeout

--- image/service-available/:ssl-tools/download.sh
+++ image/service-available/:ssl-tools/download.sh
@@ -17,11 +17,15 @@ fi
 
 LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends openssl jq
 
+echo "Download cfssl..."
 curl -o /usr/sbin/cfssl -SL https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
 chmod 700 /usr/sbin/cfssl
 
+echo "Download cfssljson..."
 curl -o /usr/sbin/cfssljson -SL https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
 chmod 700 /usr/sbin/cfssljson
+
+echo "Project sources: https://github.com/cloudflare/cfssl"
 
 # remove tools installed to download cfssl
 if [ -n "$to_install" ]; then
