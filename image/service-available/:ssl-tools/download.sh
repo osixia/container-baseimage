86392ca0c28e8776cd3c2dc28477b62b9fc8f484
Bertrand Gouny <bertrand.gouny@osixia.net>
2019-08-29 10:58:38 +0200

    cfssl multiarch support

--- image/service-available/:ssl-tools/download.sh
+++ image/service-available/:ssl-tools/download.sh
@@ -1,5 +1,34 @@
 #!/bin/bash -e
 
+UARCH=$(uname -m)
+echo "Architecture is ${UARCH}"
+
+case "${UARCH}" in
+    
+    "x86_64")
+        HOST_ARCH="amd64"
+    ;;
+    
+    "aarch64")
+        HOST_ARCH="arm64"
+    ;;
+    
+    "armv7l")
+        HOST_ARCH="arm"
+    ;;
+    
+    "i386")
+        HOST_ARCH="386"
+    ;;
+    
+    *)
+        echo "Architecture not supported. Exiting."
+        exit 1
+    ;;
+esac
+
+echo "Going to use ${HOST_ARCH} cfssl binaries"
+
 # download curl and ca-certificate from apt-get if needed
 to_install=()
 
@@ -17,12 +46,19 @@ fi
 
 LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends openssl jq
 
+# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=923479
+if [[ "${HOST_ARCH}" == 'arm' ]]; then
+    LC_ALL=C DEBIAN_FRONTEND=noninteractive c_rehash
+fi
+
 echo "Download cfssl ..."
-curl -o /usr/sbin/cfssl -SL https://github.com/osixia/cfssl/releases/download/1.3.4/cfssl_linux-amd64
+echo "curl -o /usr/sbin/cfssl -SL https://github.com/osixia/cfssl/releases/download/1.3.4/cfssl_linux-${HOST_ARCH}"
+curl -o /usr/sbin/cfssl -SL "https://github.com/osixia/cfssl/releases/download/1.3.4/cfssl_linux-${HOST_ARCH}"
 chmod 700 /usr/sbin/cfssl
 
 echo "Download cfssljson ..."
-curl -o /usr/sbin/cfssljson -SL https://github.com/osixia/cfssl/releases/download/1.3.4/cfssljson_linux-amd64
+echo "curl -o /usr/sbin/cfssljson -SL https://github.com/osixia/cfssl/releases/download/1.3.4/cfssljson_linux-${HOST_ARCH}"
+curl -o /usr/sbin/cfssljson -SL "https://github.com/osixia/cfssl/releases/download/1.3.4/cfssljson_linux-${HOST_ARCH}"
 chmod 700 /usr/sbin/cfssljson
 
 echo "Project sources: https://github.com/cloudflare/cfssl"
